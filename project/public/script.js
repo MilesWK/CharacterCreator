716e8c85bb3e08f4fb8b0ae666f9abc4f6a4b91b
MilesWK <mileswkrueger@gmail.com>
2026-07-03 10:42:19 -0400

    Picture fixed!

--- project/public/script.js
+++ project/public/script.js
@@ -1,9 +1,10 @@
 // A small little program. Let's hope this doesn't need any more work.
 // (p.s. It will.)
 
-const imgthing = document.getElementById("displayimg")
+const img_element = document.getElementById("displayimg")
 const cd = document.getElementById("chardescription")
 const errbox = document.getElementById("error")
+
 async function getRequest() {
     error.classList.add("hidden")
 
@@ -18,7 +19,9 @@ async function getRequest() {
         console.log("WHOAAAA... ")
     } else {
 
-    imgthing.src = result.result }
+    img_element.src = result.result
+    img_element.classList.remove("hidden")
+ }
 }
 
 //getRequest();   <-- Pain, Sorrow, and burned tokens 
